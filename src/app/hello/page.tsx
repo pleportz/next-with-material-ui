a111ef3e6f5e5fcfc9316bdb66efb99c26f21e35
pleportz <pleportz@gmail.com>
2024-09-12 10:57:19 +0200

    Add Autocomplete component from material ui

--- src/app/hello/page.tsx
+++ src/app/hello/page.tsx
@@ -1,7 +1,13 @@
+'use client';
+
+import { MultiSelect } from "./components/MultiSelect";
+
 export default function HelloPage() {
   return (
     <div className="p-24">
+        
       <h1>Hello Page</h1>
+      <MultiSelect />
     </div>
   );
 }
